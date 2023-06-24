6bd592ad3ed1e4e4889ebec06e2aa23306689e30
Rafael Reis <rafaelreisdeoliveira12@gmail.com>
2023-06-24 16:16:12 -0400

    Conclusão do cap 2

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # :blue_book: Desenvolvimento Real de Software
 Estudo do livro Deselvolvimento Real de Software:  Um guia de projetos para fundamentos java
 
-- :white_large_square: 1. Começando a Jornada
-- :white_large_square: 2. Análise de Extratos Bancários
+- :ballot_box_with_check: 1. Começando a Jornada
+- :ballot_box_with_check: 2. Análise de Extratos Bancários
 - :white_large_square: 3. Estendendo o Analista de Extratos Bancários
 - :white_large_square: 4. Sistema de Gerenciamento de Documentos
 - :white_large_square: 5. Motor de Regras Comerciais
---
+++ analista-extratro-bancarios/src/test/java/cap1/BankStatementCSVParserTest.java
@@ -0,0 +1,35 @@
+package cap1;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BankStatementCSVParserTest {
+	
+	private BankStatementCSVParser bankStatementCSVParser;
+	
+	@BeforeEach
+	void init() {
+		this.bankStatementCSVParser = new BankStatementCSVParser();
+	}
+
+	@Test
+	void shouldParseOneCorrectLine() {
+		final String line  = "30-01-2017,-50,Tesco";
+		
+		final BankTransaction result = bankStatementCSVParser.parseFromCSV(line);
+		
+		final BankTransaction expected = new BankTransaction(LocalDate.of(2017, Month.JANUARY, 30), -50, "Tesco");
+		
+		final double tolerance = 0.0d;
+		
+		assertEquals(expected.getDate(), result.getDate());
+		assertEquals(expected.getAmount(), result.getAmount(), tolerance);
+		assertEquals(expected.getDescription(), result.getDescription());
+ 	}
+
+}
